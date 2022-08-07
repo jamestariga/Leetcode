e3c975c7fa3195af273ffac0d0ad6053b6191112
James <jamestariga08@live.com>
2022-08-06 19:31:33 -0700

    ✨ Completed Root Equals Sum of Children

---
+++ Easy/RootEquals.js
@@ -0,0 +1,22 @@
+// const root = [10, 4, 6]
+// const arr = [5, 3, 1]
+
+// const checkTree = (root) => {
+//   let bool = true
+//   let sum = 0
+
+//   for (let i = 0; i < root.length; i++) {
+//     if (root.indexOf(root[i]) !== 0) {
+//       sum += root[i]
+//       if (sum !== root[0]) {
+//         bool = false
+//       } else {
+//         bool = true
+//       }
+//     }
+//   }
+
+//   return bool
+// }
+
+// console.log(checkTree(root))
